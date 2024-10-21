d7ff5fb1efd635d28c9191bbecdd63a996ae425e
Sudhanshu-Ambastha <sudhanshuambastha2020@gmail.com>
2024-10-21 21:38:25 +0530

    ✅done

---
+++ script.js
@@ -0,0 +1,36 @@
+let container = document.querySelector(".container");
+
+for(let i = 0; i <= 30; i++){
+    let div = document.createElement("div");
+    div.classList.add("color-container");
+    container.appendChild(div);
+}
+
+let colorContainerDiv = document.querySelectorAll(".color-container");
+console.log(colorContainerDiv);
+
+generateColors();
+
+function generateColors(){
+    colorContainerDiv.forEach((element) => {
+        let newColor = randomColor();
+        element.style.backgroundColor = "#" + newColor;
+        element.style.display = "inline-block";
+        element.style.borderRadius = "5px";
+        element.style.height = "75px";
+        element.style.width = "75px";
+        element.style.margin = "5px";
+        element.innerText = "#" + newColor;
+    });
+}
+
+function randomColor(){
+    let letters = '0123456789ABCDEF';
+    let colorCode = "";
+    let colorCodeLength = 6;
+    for(let i = 0; i < colorCodeLength; i++){
+        let randomIndex = Math.floor(Math.random() * letters.length);  
+        colorCode += letters[randomIndex];  
+    }
+    return colorCode;
+}
